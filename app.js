606e9ef7629d6ebfc1f3e1bf58dbd847348cfd72
Erasov-Ivan <124393496+Erasov-Ivan@users.noreply.github.com>
2026-01-11 05:41:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
-let tg = window.Telegram.WebApp
+const tg = window.Telegram.WebApp
+
+tg.ready();
 tg.expand();
 
+console.log(tg.initData);
 console.log(tg.initDataUnsafe);
+console.log(tg.initDataUnsafe.start_param)
 
 function send() {
     const value = document.getElementById("input").value;
